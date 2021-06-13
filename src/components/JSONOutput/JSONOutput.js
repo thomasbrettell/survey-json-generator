3fd4dd9281a176919e002ffc3510407f61099a4f
thomasbrettell <70102377+thomasbrettell@users.noreply.github.com>
2021-06-13 18:43:27 +1000

    Added snackbar to confirm copy to clipboard

--- src/components/JSONOutput/JSONOutput.js
+++ src/components/JSONOutput/JSONOutput.js
@@ -1,9 +1,23 @@
+import {useState} from 'react';
 import {CopyToClipboard} from 'react-copy-to-clipboard';
 import styles from './JSONOutput.module.css';
-import { Paper, CardHeader, Divider, IconButton } from '@material-ui/core';
+import { Paper, CardHeader, Divider, IconButton, Snackbar } from '@material-ui/core';
 import FileCopyIcon from '@material-ui/icons/FileCopy';
 
 function JSONOutput(props) {
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
+
   return (
     <Paper className={styles['json-output']} variant={'outlined'}>
       <CardHeader titleTypographyProps={{variant:'h6' }} title={'Generated JSON'} />
@@ -13,10 +27,17 @@ function JSONOutput(props) {
           variant="contained"
           color="default"
           size={'small'}
+          onClick={handleClick}
         >
           <FileCopyIcon fontSize={"small"}/>
         </IconButton>
       </CopyToClipboard>
+      <Snackbar
+        open={open}
+        autoHideDuration={2000}
+        onClose={handleClose}
+        message="Copied to clipboard"
+      />
       <Divider />
       <div className={styles['json-display']}>
         {JSON.stringify(props.json)}
